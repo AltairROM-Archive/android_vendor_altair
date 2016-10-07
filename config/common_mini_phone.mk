cf261f8b64d9bc8c8c9919008303e07749cf1c73
Griffin Millender <griffinn.millender@gmail.com>
2016-10-07 18:16:06 -0500

    cm: Rework boot animation generation
    Keep the largest bootanimation extracted
    and resize it at build-time.
    
    Change-Id: Iee32f18440ff955d13ed85b273e97cbd540721fc
    
    Conflicts:
    	bootanimation/bootanimation.tar
    	config/common.mk
    	config/common_full_phone.mk
    	config/common_full_tablet_wifionly.mk
    	config/common_full_tv.mk
    	config/common_mini_phone.mk
    	config/common_mini_tablet_wifionly.mk
    	config/common_mini_tv.mk
    	prebuilt/common/bootanimation/1080.zip
    	prebuilt/common/bootanimation/1200.zip
    	prebuilt/common/bootanimation/1440.zip
    	prebuilt/common/bootanimation/1536.zip
    	prebuilt/common/bootanimation/240.zip
    	prebuilt/common/bootanimation/320.zip
    	prebuilt/common/bootanimation/360.zip
    	prebuilt/common/bootanimation/480.zip
    	prebuilt/common/bootanimation/540.zip
    	prebuilt/common/bootanimation/600.zip
    	prebuilt/common/bootanimation/720.zip
    	prebuilt/common/bootanimation/768.zip
    	prebuilt/common/bootanimation/800.zip
    	prebuilt/common/bootanimation/halfres/1080.zip
    	prebuilt/common/bootanimation/halfres/1200.zip
    	prebuilt/common/bootanimation/halfres/1440.zip
    	prebuilt/common/bootanimation/halfres/1536.zip
    	prebuilt/common/bootanimation/halfres/1600.zip
    	prebuilt/common/bootanimation/halfres/240.zip
    	prebuilt/common/bootanimation/halfres/320.zip
    	prebuilt/common/bootanimation/halfres/360.zip
    	prebuilt/common/bootanimation/halfres/480.zip
    	prebuilt/common/bootanimation/halfres/540.zip
    	prebuilt/common/bootanimation/halfres/600.zip
    	prebuilt/common/bootanimation/halfres/720.zip
    	prebuilt/common/bootanimation/halfres/768.zip
    	prebuilt/common/bootanimation/halfres/800.zip

--- config/common_mini_phone.mk
+++ config/common_mini_phone.mk
@@ -4,9 +4,4 @@ $(call inherit-product, vendor/altair/config/common_mini.mk)
 PRODUCT_PACKAGES += \
     LatinIME
 
-ifeq ($(TARGET_SCREEN_WIDTH) $(TARGET_SCREEN_HEIGHT),$(space))
-    PRODUCT_COPY_FILES += \
-        vendor/altair/prebuilt/common/bootanimation/320.zip:system/media/bootanimation.zip
-endif
-
 $(call inherit-product, vendor/altair/config/telephony.mk)
--- config/common_mini_tablet_wifionly.mk
+++ config/common_mini_tablet_wifionly.mk
@@ -4,8 +4,3 @@ $(call inherit-product, vendor/altair/config/common_mini.mk)
 # Required Altair packages
 PRODUCT_PACKAGES += \
     LatinIME
-
-ifeq ($(TARGET_SCREEN_WIDTH) $(TARGET_SCREEN_HEIGHT),$(space))
-    PRODUCT_COPY_FILES += \
-        vendor/altair/prebuilt/common/bootanimation/800.zip:system/media/bootanimation.zip
-endif
