namdeptrai2003/android_vendor_dnd

vendor: Add a task for getting prebuilts via http
 * Gello being our first pawn
 * Use curl instead of wget (more powerful)
 * Ensure the intermediates directory exists prior to download

Change-Id: If19c18638a14a8b9ab55470ae6ee5a2deda2e3ec

build/core/config.mk
@@ -1,4 +1,5 @@
 # Copyright (C) 2015 The CyanogenMod Project
+#           (C) 2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -21,3 +22,5 @@ FRAMEWORK_CM_API_NEEDS_UPDATE_TEXT := $(TOPDIR)vendor/cm/build/core/apicheck_msg
 
 BUILD_MAVEN_PREBUILT := $(TOP)/vendor/cm/build/core/maven_artifact.mk
 PUBLISH_MAVEN_PREBUILT := $(TOP)/vendor/cm/build/core/maven_artifact_publish.mk
+
+BUILD_HTTP_PREBUILT := $(TOP)/vendor/cm/build/core/http_prebuilt.mk

AltairROM-Archive/android_vendor_altair

Conflicts:
	build/core/config.mk

build/core/config.mk
@@ -1,4 +1,5 @@
 # Copyright (C) 2016 The Altair ROM Project
+#           (C) 2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -26,3 +27,5 @@ FRAMEWORK_CM_API_NEEDS_UPDATE_TEXT := $(TOPDIR)vendor/altair/build/core/apicheck
 
 BUILD_MAVEN_PREBUILT := $(TOP)/vendor/altair/build/core/maven_artifact.mk
 PUBLISH_MAVEN_PREBUILT := $(TOP)/vendor/altair/build/core/maven_artifact_publish.mk
+
+BUILD_HTTP_PREBUILT := $(TOP)/vendor/altair/build/core/http_prebuilt.mk