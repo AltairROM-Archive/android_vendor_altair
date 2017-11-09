df67f631cfcb94ffbbbf1c98f0e600beb7334528
Martin J. Pollard <martin0566@gmail.com>
2017-11-09 12:50:50 -0500

    Update previous commit with Altair ZIP name

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -125,7 +125,7 @@ alias bib=breakfast
 function eat()
 {
     if [ "$OUT" ] ; then
-        ZIPPATH=`ls -tr "$OUT"/lineage-*.zip | tail -1`
+        ZIPPATH=`ls -tr "$OUT"/Altair-*.zip | tail -1`
         if [ ! -f $ZIPPATH ] ; then
             echo "Nothing to eat"
             return 1
