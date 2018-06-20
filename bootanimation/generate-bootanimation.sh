8bbe359cf5b5e0d7dc1f1a2873bfa410970725b9
Martin J. Pollard <martin0566@gmail.com>
2018-06-20 12:54:05 -0400

    Replaced LineageOS boot animation with Altair ROM animation

--- bootanimation/generate-bootanimation.sh
+++ bootanimation/generate-bootanimation.sh
@@ -12,17 +12,17 @@ else
 fi
 
 IMAGESCALEWIDTH="$IMAGEWIDTH"
-IMAGESCALEHEIGHT=$(expr $IMAGESCALEWIDTH / 3)
+IMAGESCALEHEIGHT="$IMAGESCALEWIDTH"
 
 if [ "$HALF_RES" = "true" ]; then
     IMAGEWIDTH=$(expr $IMAGEWIDTH / 2)
 fi
 
-IMAGEHEIGHT=$(expr $IMAGEWIDTH / 3)
+IMAGEHEIGHT="$IMAGEWIDTH"
 
 RESOLUTION=""$IMAGEWIDTH"x"$IMAGEHEIGHT""
 
-for part_cnt in 0 1 2 3 4
+for part_cnt in 0 1 2
 do
     mkdir -p $ANDROID_PRODUCT_OUT/obj/BOOTANIMATION/bootanimation/part$part_cnt
 done
@@ -30,7 +30,7 @@ tar xfp "vendor/altair/bootanimation/bootanimation.tar" -C "$OUT/bootanimation/"
 mogrify -resize $RESOLUTION -colors 250 "$OUT/bootanimation/"*"/"*".png"
 
 # Create desc.txt
-echo "$IMAGESCALEWIDTH $IMAGESCALEHEIGHT" 60 > "$OUT/bootanimation/desc.txt"
+echo "$IMAGESCALEWIDTH $IMAGESCALEHEIGHT" 15 > "$OUT/bootanimation/desc.txt"
 cat "vendor/altair/bootanimation/desc.txt" >> "$OUT/bootanimation/desc.txt"
 
 # Create bootanimation.zip
