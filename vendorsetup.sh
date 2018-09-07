d969dec35b0188051ecea58a75f875e07e1d8390
Martin Pollard <pmart303@ford.com>
2018-09-07 12:27:36 -0400

    Changed repository name

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,5 @@
 platform=gb
-for combo in $(curl -s https://raw.githubusercontent.com/Altair-Devices/devices/master/$platform/build-targets | sed -e 's/#.*$//' | grep $platform | awk '{printf "altair_%s-%s\n", $1, $2}')
+for combo in $(curl -s https://raw.githubusercontent.com/AltairROM-Devices/devices/master/$platform/build-targets | sed -e 's/#.*$//' | grep $platform | awk '{printf "altair_%s-%s\n", $1, $2}')
 do
     add_lunch_combo $combo
 done
