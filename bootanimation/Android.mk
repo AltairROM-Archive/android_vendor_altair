3cf955f043fc5f0569da86c58b1ba2d51cb9ca8a
Vasyl Gello <vasek.gello@gmail.com>
2019-07-01 13:52:35 +0000

    Fix bootanimation destpath if building off-tree
    If the build is invoked off-tree, like:
    
        . build/envsetup.sh
        export OUT_DIR=/some/other/dir/out
        export DIST_DIR=/some/other/dir/dist
        breakfast chagalllte
        mka \
            target-files-package dist
    
    the bootanimation.zip is correctly generated in $OUT_DIR.
    
    However, if the user overrides directories after breakfast, i.e:
    
        . build/envsetup.sh
        breakfast chagalllte
        mka \
            OUT_DIR=/some/other/dir/out \
            DIST_DIR=/some/other/dir/dist \
            target-files-package dist
    
    the destination path of intermediate bootanimation.zip is still
    controlled by $ANDROID_PRODUCT_OUT which is set by build/envsetup.sh.
    This leads to a copy error and a failed build.
    
    The fix overrides $ANDROID_PRODUCT_OUT by the always-known PRODUCT_OUT
    (make variable defined in build/core/envsetup.mk), making it error-prone
    to environment variable declaration.
    
    Also, the "sh" invocation is replaced with generic make-variable
    $(shell).
    
    Change-Id: Ic94d1b538fc01946b628f9b5d776548a03acde97
    Signed-off-by: Vasyl Gello <vasek.gello@gmail.com>

--- bootanimation/Android.mk
+++ bootanimation/Android.mk
@@ -16,7 +16,8 @@
 #
 
 define build-bootanimation
-    sh vendor/altair/bootanimation/generate-bootanimation.sh \
+    $(shell) vendor/altair/bootanimation/generate-bootanimation.sh \
+    $(PRODUCT_OUT) \
     $(TARGET_SCREEN_WIDTH) \
     $(TARGET_SCREEN_HEIGHT) \
     $(TARGET_BOOTANIMATION_HALF_RES)
