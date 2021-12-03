dcc471d60443b065d0662e24afee9ea1b2acd58c
Martin J. Pollard <martin0566@gmail.com>
2021-12-03 15:48:47 -0500

    Revert "config: Enable window blur on most devices"
    This reverts commit cab39dc29c6455548afc7015b860ab8b09a22121.

--- config/altair.mk
+++ config/altair.mk
@@ -1,8 +1,1 @@
 # Altair ROM specific configuration
-
-# Blur
-ifneq ($(TARGET_DISABLE_BLUR), true)
-PRODUCT_PRODUCT_PROPERTIES += \
-    ro.sf.blurs_are_expensive=1 \
-    ro.surface_flinger.supports_background_blur=1
-endif
